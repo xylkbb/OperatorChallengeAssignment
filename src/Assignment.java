76c2158e272cf4d40bbf740809473d26bb325627
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-09 13:41:08 -0400

    Intial Commit2

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        double v1 = 20.00;
+        double v2 = 80.00;
+        double v = (v1 + v2) * 100.00;
+        double r = v % 40.00;
+        boolean noR = (r == 0.00) ? true : false;
+        System.out.println("isNoRemainder = " + noR);
+        if(!noR) {
+            System.out.println("Got some remainder");
+        }
+
+
+
+    }
 }
